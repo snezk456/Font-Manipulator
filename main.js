13fe26959f280a14f3482c636d99ca636e8e4ce5
snezk456 <86143353+snezk456@users.noreply.github.com>
2022-12-18 14:38:41 +0530

    Update main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 550);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    background('#0000FF');
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialized');
+}
+
+function gotPoses() {
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
