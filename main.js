9825b1fb7d447be8a93a7821093e401f0d068404
snezk456 <86143353+snezk456@users.noreply.github.com>
2023-01-22 14:39:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+rightWristY = 0;
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 550);
@@ -10,16 +15,30 @@ function setup() {
 }
 
 function draw() {
-    background('#0000FF');
+    background('#808080');
+
+    document.getElementById("square_side".innerHTML = "Width And Height of a Square will be = " + difference +"px");
+    fill('#FF007F');
+    stroke('#0000FF');
+    square(noseX, noseY, difference);
 }
 
 function modelLoaded() {
     console.log('PoseNet is initialized');
 }
 
-function gotPoses() {
+function gotPoses(results) {
     if(results.length > 0)
     {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+
+        console.log("leftWristX = " + leftWristX + " rightWristX = "+ rightWristX + " difference = " + difference);
     }
 }
